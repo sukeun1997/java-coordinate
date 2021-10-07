bda242e1dd6e140d296333eebebb50628c6f78bb
sukeun1997 <busan-sy@naver.com>
2021-10-07 15:48:05 +0900

    feat(좌표계산기:삼각형 면적) : 삼각형 유효성 검사 , 삼각형 면적 구하기 완성

--- src/main/java/Coordinate/Model/TRIANGLE.java
+++ src/main/java/Coordinate/Model/TRIANGLE.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class TRIANGLE extends AbstractFigure {
 
     private final static String AREA_INFO = "삼각형 넓이는 ";
+    private final static String ERROR_MESSAGE = "삼각형이 아닙니다.";
 
     public TRIANGLE(List<Point> points) {
         super(points);
@@ -16,10 +17,11 @@ public class TRIANGLE extends AbstractFigure {
 
     @Override
     public double area() {
-        double a = ConvertToLine(getPoints()).get(0);
-        double b = ConvertToLine(getPoints()).get(1);
-        double c = ConvertToLine(getPoints()).get(2);
-        double s = LineSum(ConvertToLine(getPoints()));
+        List<Double> lineList = ConvertToLine(getPoints());
+        double a = lineList.get(0);
+        double b = lineList.get(1);
+        double c = lineList.get(2);
+        double s = LineSum(lineList);
         return Math.round(Math.sqrt(s * (s - a) * (s - b) * (s - c)));
     }
 
@@ -33,11 +35,23 @@ public class TRIANGLE extends AbstractFigure {
         line.add(points.get(0).getSqrt(points.get(1)));
         line.add(points.get(1).getSqrt(points.get(2)));
         line.add(points.get(0).getSqrt(points.get(2)));
+        CheckTriangle(line);
         return line;
     }
 
     private Double LineSum(List<Double> line) {
         return line.stream().mapToDouble(Number::doubleValue).sum() / 2;
     }
+
+    private void CheckTriangle(List<Double> line){
+        if (2 * getMaxLine(line) < LineSum(line).intValue()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    private int getMaxLine(List<Double> line) {
+        return line.stream().mapToInt(Double::intValue).max().getAsInt();
+    }
+
 
 }
